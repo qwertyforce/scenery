019a52b450152fad2f146509aad6e0344031103c
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-08-31 21:45:49 +0300

    material-ui typescript template

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,9 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {
+            // suppress errors for missing 'import React' in files
+   "react/react-in-jsx-scope": "off",
+   // allow jsx syntax in js files (for next.js project)
+  "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx",".ts", ".tsx"] }], //should add ".ts" if typescript project
     }
 };
--- pages/about.tsx
+++ pages/about.tsx
@@ -6,16 +6,14 @@ import ProTip from '../components/ProTip';
 import Link from '../components/Link';
 import Copyright from '../components/Copyright';
 
-export default function Index() {
+export default function About() {
   return (
     <Container maxWidth="sm">
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
           Next.js with TypeScript example
         </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
+        <Link href="/">Go to the main page</Link>
         <ProTip />
         <Copyright />
       </Box>
