d77de40d6046c31728b66b600bd2a380e7218123
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-09-11 16:05:32 +0300

    stats page

---
+++ pages/stats.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import db_ops from '../server/helpers/db_ops'
+import AppBar from '../components/AppBar'
+import {GetStaticProps} from 'next'
+
+export default function Index(props) {
+  return (
+    <div>
+      <AppBar/>
+      <div>Total Images: {props.number_of_images}</div>
+      <div>Total Authors: {props.number_of_authors}</div>
+      <div>Total tags: {props.number_of_tags}</div>
+      </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+   const imgs=await db_ops.image_ops.get_all_images()
+   const authors=new Set()
+   const tags=new Set()
+   for(const img of imgs){
+     authors.add(img.author)
+     for (const tag of img.tags){
+       tags.add(tag)
+     }
+   }
+   return {
+    props:{
+      number_of_images:imgs.length,
+      number_of_authors:authors.size,
+      number_of_tags:tags.size,
+    }
+  }
+} 
+ 
