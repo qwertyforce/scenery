a93559a6372abceadd5ca8781cb18be643c27938
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-09-12 21:02:48 +0300

    appbar fix

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -29,7 +29,7 @@ const useStyles = makeStyles((theme) => ({
       backgroundColor: fade(theme.palette.common.white, 0.25),
     },
     marginRight: theme.spacing(2),
-    marginLeft: 0,
+    marginLeft: theme.spacing(2),
     width: '100%',
     [theme.breakpoints.up('sm')]: {
       marginLeft: theme.spacing(3),
