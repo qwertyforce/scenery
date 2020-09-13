c163aa6c5dbd45692c366f5aa709b4392818eed0
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-09-13 18:32:29 +0300

    about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,21 +2,22 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import ProTip from '../components/ProTip';
-import Link from '../components/Link';
-import Copyright from '../components/Copyright';
+import AppBar from '../components/AppBar'
 
 export default function About() {
   return (
-    <Container maxWidth="sm">
+    <div>
+      <AppBar/>
+      <Container maxWidth="sm">
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Next.js with TypeScript example
+          OnlyComfy.art
         </Typography>
-        <Link href="/">Go to the main page</Link>
-        <ProTip />
-        <Copyright />
+        Only high quality comfy pony scenery art.
+        <br/>
+        onlycomfyart@gmail.com
       </Box>
     </Container>
+    </div>
   );
 }
