978685ebd90ef6136628a5e962c489368ef34238
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-09-21 19:29:21 +0300

    rename

--- server/bulk_calculate_color_hist_and_similarities/calculate_color_hist.ts
+++ server/bulk_calculate_color_hist_and_similarities/calculate_color_hist.ts
@@ -23,7 +23,6 @@ const histAxes:HistAxes[]= [
 const PATH_TO_IMAGES = path.join("..", "..", "..", 'public', 'images')
 
 async function calculate_color_hist(){
-  return
     const images = await db_ops.image_ops.get_all_images()
     for(const image of images){
         console.log(image.id)
@@ -38,29 +37,33 @@ async function calculate_color_hist(){
     process.exit()
 }
 
-async function clac() {
+async function calc_similarities() {
   const s= await db_ops.image_search.get_color_similarities_by_id(4)
   for(const x of s){
     console.log(x)
   }
-  // const images = await db_ops.image_ops.get_all_images() 
-  // const get_all_hist=await db_ops.image_search.get_all_color_hists()
-  // console.time();
-  // for (let i = 0; i < images.length - 1; i++) {
-  //   const target_image = get_all_hist.find((el)=>el.id===images[i].id)
-  //   const target_hist = new cv.Mat(target_image.color_hist, cv.CV_32F)
-  //   const similarities=[]
-  //   for (let j = i + 1; j < images.length; j++) {
-  //     const _image = get_all_hist.find((el)=>el.id===images[j].id)
-  //     const color_hist_mat = new cv.Mat(_image.color_hist, cv.CV_32F);
-  //     const similarity = await target_hist.compareHistAsync(color_hist_mat, cv.HISTCMP_INTERSECT)
-  //     similarities.push({id:images[j].id,similarity:similarity})
-  //     color_hist_mat.release()
-  //     console.log(`${i}->${j}`)
-  //   }
-  //   await db_ops.image_search.add_color_similarities_by_id(images[i].id,similarities)
-  // }
-  // console.timeEnd()
+  const images = await db_ops.image_ops.get_all_images() 
+  const get_all_hist=await db_ops.image_search.get_all_color_hists()
+  console.time();
+  for (let i = 0; i < images.length - 1; i++) {
+    const target_image = get_all_hist.find((el)=>el.id===images[i].id)
+    const target_hist = new cv.Mat(target_image.color_hist, cv.CV_32F)
+    const similarities=[]
+    for (let j = i + 1; j < images.length; j++) {
+      const _image = get_all_hist.find((el)=>el.id===images[j].id)
+      const color_hist_mat = new cv.Mat(_image.color_hist, cv.CV_32F);
+      const similarity = await target_hist.compareHistAsync(color_hist_mat, cv.HISTCMP_INTERSECT)
+      similarities.push({id:images[j].id,similarity:similarity})
+      color_hist_mat.release()
+      console.log(`${i}->${j}`)
+    }
+    await db_ops.image_search.add_color_similarities_by_id(images[i].id,similarities)
+  }
+  console.timeEnd()
+  process.exit()
 }
-clac()
-calculate_color_hist()
+async function calc_color_hists_and_similarities() {
+  await calculate_color_hist()
+  await calc_similarities()
+}
+calc_color_hists_and_similarities()
