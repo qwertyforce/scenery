2d8ad3af9e0a85dc45736fe3290aff7f131de236
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-09-22 20:35:34 +0300

    tools for color hist/sim

---
+++ server/bulk_calculate_color_hist_and_similarities/delete_color_hists_and_similarities.ts
@@ -0,0 +1,10 @@
+import db_ops from '../helpers/db_ops'
+
+async function delete_color_hists_and_similarities() {
+//  const color_hist_image_ids=[]
+ const similarities_image_ids=[1]
+ for (const id of similarities_image_ids){
+   await db_ops.image_search.delete_id_from_color_similarities(id)
+ }
+}
+delete_color_hists_and_similarities()
