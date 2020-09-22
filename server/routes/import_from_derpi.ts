c574898ae38f4895ae321499aa6caac9ea21b2f1
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-09-22 20:43:56 +0300

    Update import_from_derpi.ts

--- server/routes/import_from_derpi.ts
+++ server/routes/import_from_derpi.ts
@@ -51,6 +51,8 @@ async function import_from_derpi(req: Request, res: Response) {
                 const parsed_author = await parse_author(derpi_data.tags)
                 const derpi_link = "https://derpibooru.org/images/" + derpi_data.id
                 const phash = await imghash.hash(image.data, 16);
+                derpi_data.tags.push(`width:${derpi_data.width}`)
+                derpi_data.tags.push(`height:${derpi_data.height}`)
                 await image_ops.calculate_color_hist_and_similarities(new_image_id,image.data)
                 await db_ops.image_ops.add_image(new_image_id, derpi_data.format.toLowerCase(), derpi_data.width, derpi_data.height, parsed_author, derpi_data.size,
                     derpi_link, derpi_data.upvotes, derpi_data.downvotes, derpi_data.id, derpi_data.created_at,
