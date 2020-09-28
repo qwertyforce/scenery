a2bb83be02298db9ebcb578aea9e0bfc481afb4e
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-09-28 19:49:56 +0300

    downscale thumbnails

--- server/webp_converter/converter.ts
+++ server/webp_converter/converter.ts
@@ -11,10 +11,24 @@ async function convert() {
     for (const image_file_name of IMAGES) {
         if(!WEBP_IMAGES.includes(`${path.parse(image_file_name).name}.webp`)){
             console.log(`converting ${image_file_name}`)
-            const data = await sharp(`${PATH_TO_IMAGES}/${image_file_name}`)
+            const metadata = await sharp(`${PATH_TO_IMAGES}/${image_file_name}`).metadata()
+            if(metadata && metadata.height && metadata.width){
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                const x:any ={}
+                if(metadata.width>metadata.height){
+
+                    x.width=Math.min(metadata.width,750)
+                }else if(metadata.width<metadata.height){
+                    x.height=Math.min(metadata.height,750)
+                }else{
+                    x.width=Math.min(metadata.width,750)
+                }
+                const data = await sharp(`${PATH_TO_IMAGES}/${image_file_name}`).resize(x)
                 .webp({ quality: 80, reductionEffort: 6 })
-                .toFile(`../webp_images/${path.parse(image_file_name).name}.webp`);
+                .toFile(`${PATH_TO_WEBP_IMAGES}/${path.parse(image_file_name).name}.webp`);
             console.log(data)
+            }
+            
         }   
     }
 }
