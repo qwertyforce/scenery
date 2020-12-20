d757cdb36a222a5152d172b030d7d0dbab8169f8
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-12-20 12:26:14 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
     ],
     "rules": {
             // suppress errors for missing 'import React' in files
+    "@typescript-eslint/explicit-module-boundary-types": "off",
    "react/react-in-jsx-scope": "off",
    // allow jsx syntax in js files (for next.js project)
   "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx",".ts", ".tsx"] }], //should add ".ts" if typescript project
