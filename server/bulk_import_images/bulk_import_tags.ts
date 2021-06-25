255f73f1c1deea00a3cc6ca07cf8dc23a53dc587
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2021-06-25 17:35:15 +0300

    Create bulk_import_tags.ts

---
+++ server/bulk_import_images/bulk_import_tags.ts
@@ -0,0 +1,15 @@
+import fs from 'fs'
+import path from 'path'
+import db_ops from "../helpers/db_ops"
+import config from "../../config/config"
+const fsPromises = fs.promises;
+
+async function import_tags() {
+    const img_tags=JSON.parse(await fsPromises.readFile(path.join(config.root_path,"ambience","id_tags.txt"),'utf8'))
+    for (const img of img_tags) {
+       console.log(img.id)
+       await db_ops.image_ops.set_tags_to_image_by_id(img.id,img.tags)
+    }
+    process.exit()
+}
+import_tags()
