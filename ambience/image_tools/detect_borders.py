e53f150485633cd5db8a616c8e773f37493678dc
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2021-07-11 21:55:15 +0300

    image_tools

--- ambience/image_tools/detect_borders.py
+++ ambience/image_tools/detect_borders.py
@@ -2,23 +2,32 @@ from os import listdir
 from PIL import Image
 from PIL import Image, ImageChops
 from joblib import Parallel, delayed
-
+IMG_PATH="./../../import/images/"
 def trim(im):
     bg = Image.new(im.mode, im.size, im.getpixel((0,0)))
     diff = ImageChops.difference(im, bg)
-    diff = ImageChops.add(diff, diff, 2.0, -15)
+    diff = ImageChops.add(diff, diff, 2.0, -25)
     bbox = diff.getbbox()
     if bbox:
         if bbox[0]==0 and bbox[1]==0 and bbox[2]==im.size[0] and bbox[3]==im.size[1]:
             return False
+        if bbox[1]!=0:
+            bottom_margin=im.size[1]-bbox[3]
+            if bottom_margin==0:
+                return False
+        if bbox[3]!=im.size[1]:
+            top_margin=bbox[1]
+            if top_margin==0:
+                return False
         return True
     return False
+
 def check_borders(file_name):
-    im = Image.open("./../import/images/"+file_name)
+    im = Image.open(IMG_PATH+file_name)
     if trim(im):
         return file_name
 
-files=listdir("./../import/images")
+files=listdir(IMG_PATH)
 borders=Parallel(n_jobs=-1,verbose=1)(delayed(check_borders)(file_name) for file_name in files)
 borders= [i for i in borders if i] #remove None's
 print(borders)
