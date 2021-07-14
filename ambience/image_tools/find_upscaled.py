b42bc95ae7356631c143ca9a0a01506312766f47
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2021-07-14 22:12:48 +0300

    Create find_upscaled.py

---
+++ ambience/image_tools/find_upscaled.py
@@ -0,0 +1,23 @@
+from os import listdir
+import subprocess
+from tqdm import tqdm
+import re
+IMG_PATH="./../../testing/reddit"
+files=listdir(IMG_PATH)
+all=[]
+for file in tqdm(files):
+    file_path=f"{IMG_PATH}/{file}"
+    result = subprocess.run(f"resdet {file_path}",shell=True, stdout=subprocess.PIPE)
+    res_str=result.stdout.decode("utf-8").splitlines()
+    if len(res_str)<2:
+        continue
+    if "(not upsampled)" in res_str[1]:
+        continue
+    given_resolution=re.findall(r'\d+', res_str[0])
+    given_pixels_total=int(given_resolution[0])*int(given_resolution[1])
+    best_guess_resolution=re.findall(r'\d+', res_str[1])
+    best_pixels_total=int(best_guess_resolution[0])*int(best_guess_resolution[1])
+    if best_pixels_total/given_pixels_total<0.6:
+        all.append(file_path)
+print(all)
+print(len(all))
