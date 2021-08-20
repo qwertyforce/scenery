b62b9c8f9e763188b30444e11cbcc9260656fef6
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2021-08-20 19:20:02 +0300

    brotli

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 require('sharp') //https://github.com/lovell/sharp/issues/2655#issuecomment-815684743
+const zlib = require("zlib")
+const CompressionPlugin = require("compression-webpack-plugin")
+const use_brotli = false
 module.exports = {
   poweredByHeader: false,
   compress: false,
@@ -11,6 +15,23 @@ module.exports = {
         destination: '/last_added/1'
       }
     ]
+  },
+  webpack: (config, { isServer }) => {
+    if (use_brotli && !isServer) {
+      config.plugins.push(
+        new CompressionPlugin({
+          filename: "[path][base].br",
+          algorithm: "brotliCompress",
+          test: /\.(js|css|html)$/,
+          compressionOptions: {
+            params: {
+              [zlib.constants.BROTLI_PARAM_QUALITY]: 1,
+            },
+          }
+        })
+      )
+    }
+    return config
   },
   env: {  //https://nextjs.org/docs/api-reference/next.config.js/environment-variables
     recaptcha_site_key: "6LcqV9QUAAAAAEybBVr0FWnUnFQmOVxGoQ_Muhtb",
