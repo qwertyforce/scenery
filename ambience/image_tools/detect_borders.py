0ab8fbd28a7c4d4e71e6ba04cb272b18925655a6
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2021-08-27 21:43:12 +0300

    Update detect_borders.py

--- ambience/image_tools/detect_borders.py
+++ ambience/image_tools/detect_borders.py
@@ -3,13 +3,14 @@ from PIL import Image
 from PIL import Image, ImageChops
 from joblib import Parallel, delayed
 IMG_PATH="./../../import/images/"
-def trim(im):
+
+def _detect_borders(im):
     bg = Image.new(im.mode, im.size, im.getpixel((0,0)))
     diff = ImageChops.difference(im, bg)
     diff = ImageChops.add(diff, diff, 2.0, -25)
     bbox = diff.getbbox()
     if bbox:
-        if bbox[0]==0 and bbox[1]==0 and bbox[2]==im.size[0] and bbox[3]==im.size[1]:
+        if bbox[0]==0 and bbox[1]==0 and bbox[2]==im.size[0] and bbox[3]==im.size[1]: #bbox didn't crop anything
             return False
         if bbox[1]!=0:
             bottom_margin=im.size[1]-bbox[3]
@@ -22,13 +23,13 @@ def trim(im):
         return True
     return False
 
-def check_borders(file_name):
+def detect_borders(file_name):
     im = Image.open(IMG_PATH+file_name)
-    if trim(im):
+    if _detect_borders(im):
         return file_name
 
 files=listdir(IMG_PATH)
-borders=Parallel(n_jobs=-1,verbose=1)(delayed(check_borders)(file_name) for file_name in files)
+borders=Parallel(n_jobs=-1,verbose=1)(delayed(detect_borders)(file_name) for file_name in files)
 borders= [i for i in borders if i] #remove None's
 print(borders)
 print(len(borders))
